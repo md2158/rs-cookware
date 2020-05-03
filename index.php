e367092550eedb49b86beb2cc7e08053b164e7a1
Meret D <meret.danatarov@yandex.ru>
2020-05-04 01:24:06 +0300

    header styling continued

--- index.php
+++ index.php
@@ -4,18 +4,88 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Lovince</title>
+  <title>Cookware</title>
   <link rel="icon" href="img/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="fonts/cookware-icons/styles.css">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
 </head>
 <body>
 
   <!-- Шапка -->
   <header>
-    <div class="rs-cookware-header"></div>
+    <div class="rs-cookware-header">
+      <div class="rs-cookware-header__top-menu">
+        <div class="container">
+          <div class="rs-cookware-header__top-menu_nav">
+            <a class="rs-cookware-header__top-menu_link">
+              <i class="icon icon-user"></i>
+              <span>Войти</span>
+            </a>
+            <a href="tel:78122428072" class="rs-cookware-header__top-menu_link">
+              <i class="icon icon-phone"></i>
+              <span>+7 (812) 242-80-72</span>
+            </a>
+            <a href="tel:78003333406" class="rs-cookware-header__top-menu_link">
+              <i class="icon icon-phone"></i>
+              <span>+7 (800) 333-34-06</span>
+            </a>
+            <a href="mailto:info@ChugunPRO.ru" class="rs-cookware-header__top-menu_link">
+              <i class="icon icon-envelope"></i>
+              <span>info@ChugunPRO.ru</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="rs-cookware-header__main-nav">
+        <div class="container">
+          <div class="rs-cookware-header__main-nav_content">
+            <a class="rs-cookware-header__main-nav_logo">
+              <img src="img/svg/logo.svg" alt="logo-cookware">
+            </a>
+            <nav class="rs-cookware-header__menu">
+              <ul class="primary-menu">
+                <li class="primary-menu__item"><a class="primary-menu__link" href="#!">Главная</a></li>
+                <li class="primary-menu__item sub-menu-parent">
+                  <a class="primary-menu__link" href="#!">Каталог</a>
+                  <ul class="sub-menu">
+                    <li class="sub-menu__item"><a class="sub-menu__link" href="#!">Каталог 1</a></li>
+                    <li class="sub-menu__item"><a class="sub-menu__link" href="#!">Каталог 2</a></li>
+                    <li class="sub-menu__item"><a class="sub-menu__link" href="#!">Каталог 3</a></li>
+                    <li class="sub-menu__item"><a class="sub-menu__link" href="#!">Каталог 4</a></li>
+                  </ul>
+                </li>
+                <li class="primary-menu__item"><a class="primary-menu__link" href="#!">Покупателю</a></li>
+                <li class="primary-menu__item"><a class="primary-menu__link" href="#!">Новости/Акции</a></li>
+                <li class="primary-menu__item"><a class="primary-menu__link" href="#!">О магазине</a></li>
+                <li class="primary-menu__item"><a class="primary-menu__link" href="#!">Доставка</a></li>
+                <li class="primary-menu__item"><a class="primary-menu__link" href="#!">Контакты</a></li>
+              </ul>
+            </nav>
+            <div class="rs-cookware-header__main-nav_icons">
+              <div class="rs-cookware-header__main-nav_icons_cart">
+                <a href="#!">
+                  <i class="icon icon-cart"></i>
+                </a>
+                <span>0</span>
+              </div>
+              <a href="#!" id="searchModalBtn">
+                <i class="icon icon-search"></i>
+              </a>
+              <div class="rs-cookware-header__search animated fadeIn" id="searchModal">
+                <input class="rs-cookware-header__search_input" type="text" name="search" placeholder="Поиск...">
+                <button class="rs-cookware-header__search_button"><i class="icon icon-search"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
+  <div class="slider">
+    <img class="w-100" src="img/slider-1.jpg" alt="">
+  </div>
   <!-- Каталог -->
   <section class="rs-cookware-catalog"></section>
   <!-- Хиты продаж -->
--- js/common.js
+++ js/common.js
@@ -1,75 +1,20 @@
-// function linkHover(linkClass) {
-//     $('.'+linkClass).mouseenter(function(){
-//         // $('.'+linkClass).css('opacity', '0.5');
-//         $('.'+linkClass).addClass('non-active');
-//         $(this).removeClass('non-active');
-//         $(this).addClass('active');
-//     });
-//     $('.'+linkClass).mouseleave(function(){
-//         $('.'+linkClass).removeClass('non-active');
-//     });
-// }
-
-// Mobile menu with child function
-// function menuWithChild(parentMenu, childMenu) {
-//     $('.'+parentMenu).click(function(){
-//         $('.'+parentMenu).toggleClass('active');
-//         $('.'+childMenu).toggleClass('d-block');
-//     });
-//     $('.'+parentMenu).parent().siblings().click(function(){
-//         $('.'+childMenu).removeClass('d-block');
-//         $('.'+parentMenu).removeClass('active');
-//     });
-// }
-
 $(document).ready(function ($) {
-    // menu hover effect
-    // linkHover('menu-nav__item');
-    // linkHover('menu-nav__submenu_item');
-    // linkHover('menu-nav__submenu-child_link');
-    
-    // Mobile Menu
-    // $('#mobileMenuBtn').click(function(){
-    //     $(this).toggleClass('active');
-    //     $(this).find('.animated-menu-icon').toggleClass('open');
-    //     $('.rs-quartet-header__menu').toggleClass('active');
-    // });
-
-    // if ($(window).width() < 1200) {
-    //     menuWithChild('menu-nav__link_has-child','menu-nav__submenu');
-    //     menuWithChild('menu-nav__submenu_has-child','menu-nav__submenu-child');
-    // }
-    $("#openSearchModal").click(function(){
-        $('#searchModal').addClass('show');
-        setTimeout(function(){
-            $('#searchModal').addClass('active');
-            $('#searchModal input').focus();
-        }, 100);
-    });
-    $("#closeSearchModal").click(function(){
-        $('#searchModal').removeClass('active');
-        setTimeout(function(){
-            $('#searchModal').removeClass('show');
-        }, 500);
-    });
-    // Mobile Menu
-    $('#mobileMenuBtn').click(function(){
-        $(this).toggleClass('active');
-        $(this).find('.animated-menu-icon').toggleClass('open');
-        $('.rs-quartet-header__menu').toggleClass('active');
+    $("#searchModalBtn").click(function(){
+        $(this).find('.icon').toggleClass('icon-search icon-cross');
+        $('#searchModal').toggleClass('view');
     });
     // Mobile menu list
     if ($(window).width() < 992) {
-        var menu = '';
-        $( ".rs-lowence-header__menu" ).each(function() {
-            // console.log(menu);
-            menu += $(this).html();
-        });
-        $('#mobileMenuModal .modal-content').html(menu);
-        $('.rs-lowence-header__menu').html('');
-        $('#mobileMenuModal').on('hide.bs.modal', function (e) {
-            $('#mobileMenuBtn').removeClass('active');
-            $('#mobileMenuBtn .animated-menu-icon').removeClass('open');
-        })
+        // var menu = '';
+        // $( ".rs-lowence-header__menu" ).each(function() {
+        //     // console.log(menu);
+        //     menu += $(this).html();
+        // });
+        // $('#mobileMenuModal .modal-content').html(menu);
+        // $('.rs-lowence-header__menu').html('');
+        // $('#mobileMenuModal').on('hide.bs.modal', function (e) {
+        //     $('#mobileMenuBtn').removeClass('active');
+        //     $('#mobileMenuBtn .animated-menu-icon').removeClass('open');
+        // })
     };
 });
